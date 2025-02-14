4fef88bae9f34583ca225a8516d09a06bc3644a2
aeriall2021 <bear@aeriall.com>
2025-02-13 21:13:28 -0600

    Marking Spot No Graphics

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ window.addEventListener('scroll', function() {
     const heroVideo = document.getElementById('hero-video');
     if (heroVideo) {
       let scrollPosition = window.pageYOffset;
-      // Adjust the multiplier (0.5) to control parallax speed
       heroVideo.style.transform = 'translate(-50%, -50%) translateY(' + scrollPosition * 0.5 + 'px)';
     }
   });
